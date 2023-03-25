1194b8592491cd07d6f3d7fdd3be4a8877c32b3f
Didgitalpunk <didgitalpunk@gmail.com>
2023-03-25 21:42:56 +0100

    removed beta.ino, beta.cpp, and pushed Mega_board_tester__program to V1.2, with board detection! only uno and mega board for now, more will come.

--- MEGA_board_tester__program/MEGA_board_tester_program.ino
+++ MEGA_board_tester__program/MEGA_board_tester_program.ino
@@ -1,7 +1,20 @@
-//Version 1.1 ---- 3/25/2023
+//Version 1.2    3/25/2023
+//Credits to Freya Mutschler and Diana N. Corsten
 
+#define MIN 0
 
-int LEDmega[] = {
+#if defined(__AVR_ATmega328P__)
+#define MAX 19
+#define Shift 4
+#define Delay 100
+int pins[MAX+1] = {  7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14, 13, 12 , 11, 10, 9, 8 };
+#endif
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+#define MAX 69
+#define Shift 6
+#define Delay 75
+const int pins[MAX+1] = {
 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2,
 1, 0, 14, 15, 16, 17, 18, 19,
 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
@@ -9,55 +22,41 @@ int LEDmega[] = {
 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
 50, 51, 52, 53,
 69, 68, 67, 66, 65, 64,
-63, 62, 61, 60, 59, 58, 57, 56, 55, 54
-};
+63, 62, 61, 60, 59, 58, 57, 56, 55, 54};
+#endif
 
-int LEDmegaoff[] = {
-59, 58, 57, 56, 55, 54,
-13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2,
-1, 0, 14, 15, 16, 17, 18, 19,
-20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-50, 51, 52, 53,
-69, 68, 67, 66, 65, 64,
-63, 62, 61, 60
-};
+const int* pin = &pins[0];
 
-int LEDuno[] = {  7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14, 13, 12 , 11, 10, 9, 8 };
-
-int LEDunooff[] = { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14 };
-
-void setup() {
-  
+int increment(int val, int inc) {
+    if (val + inc <= MAX) { return val + inc; }
+    else { return MIN + ((val + inc) % MAX-1); }
 }
+int decrement(int val, int dec) {
+    if (val - dec >= MIN) { return val - dec; }
+    else {return MAX - (val-dec+1)*-1;}
+}
+void pointerincrement(int inc) {
+    int val = (pin - pins);
+    if (val + inc <= MAX) { pin = &pins[val + inc]; }
+    else { pin = &pins[MIN + ((val + inc) % MAX - 1)]; }
+}
+void pointerdecrement(int dec) {
+    int val = (pin - pins);
+    if (val - dec >= MIN) { pin = &pins[val - dec]; }
+    else { pin = &pins[MAX - (val - dec + 1) * -1]; }
+}
+
+
+void setup() {}
 
 void loop() {
-
-  int x = 0;
-  int type = 1 ;//0 for mega, 1 for uno
-  
-while(type == 0){
-  for (x = 0; x <= 69; x++) {
-    pinMode(LEDmega[x], OUTPUT);
-    digitalWrite(LEDmega[x], LOW);
-    digitalWrite(LEDmegaoff[x], LOW);
-    digitalWrite(LEDmega[x], HIGH);
-    delay(50);
+int  x = 0;
+  for (x = MIN; x <= MAX; x++) {
+    pinMode(pins[x], OUTPUT);
+    digitalWrite(pins[x], LOW);
+    digitalWrite(pins[decrement(x,Shift)], LOW);
+    digitalWrite(pins[x], HIGH);
+    delay(Delay);
   }
   if (x > 69) { x = 0; }
-}
-
-while(type == 1){
-    int x = 0;
-  for (x = 0; x <= 19; x++) {
-    pinMode(LEDuno[x], OUTPUT);
-    digitalWrite(LEDuno[x], LOW);
-    digitalWrite(LEDunooff[x], LOW);
-    digitalWrite(LEDuno[x], HIGH);
-    delay(75);
-  }
-  if (x > 19) { x = 0; }
-  
-}
 }
