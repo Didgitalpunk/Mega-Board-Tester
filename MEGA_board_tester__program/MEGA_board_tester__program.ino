83b4c25a534e8344a8e4f3eeb45954c878ef9b82
Didgitalpunk <didgitalpunk@gmail.com>
2023-03-28 23:08:32 +0200

    PCB updated to version 2 added  ∅D20, ∅D21, ∅D38, to ensure compatibility with Arduino Zero boards. (D38 is called ANT on the board, unnamed on mega and unoR3) moved the ISTY logo and credits to make place for new components added a QR code linking to the repo on the back updated version on board tweaked placement of a few component names. OCD.
    have yet to decide on what to do with D0 and D1 so they don't interfere with program upload. leave them? remove them and put in test loops for probing? tbd.

--- MEGA_board_tester__program/MEGA_board_tester__program.ino
+++ MEGA_board_tester__program/MEGA_board_tester__program.ino
@@ -1,19 +1,22 @@
-//Version 1.2    3/25/2023
+//Version 1.2.1    3/27/2023
 //Credits to Freya Mutschler and Diana N. Corsten
 
-#define MIN 0
+constexpr auto MIN = 0;
 
 #if defined(__AVR_ATmega328P__)
-#define MAX 19
-#define Shift 4
-#define Delay 100
+//arduino uno (R1, R2, R3), arduino nano
+constexpr auto MAX = 19;
+constexpr auto Shift = 4;
+constexpr auto Delay = 100;
 int pins[MAX+1] = {  7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14, 13, 12 , 11, 10, 9, 8 };
 #endif
 
+
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
-#define MAX 69
-#define Shift 6
-#define Delay 75
+//arduino Mega
+constexpr auto MAX = 69;
+constexpr auto Shift = 6;
+constexpr auto Delay = 75;
 const int pins[MAX+1] = {
 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2,
 1, 0, 14, 15, 16, 17, 18, 19,
@@ -25,7 +28,19 @@ const int pins[MAX+1] = {
 63, 62, 61, 60, 59, 58, 57, 56, 55, 54};
 #endif
 
-const int* pin = &pins[0];
+
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
+//arduino Leonardo
+#endif
+
+
+#if defined(?)
+//arduino Zero
+constexpr auto MAX = 21;
+constexpr auto Shift = 4;
+constexpr auto Delay = 100;
+int pins[MAX+1] = { 21, 20, 13, 12 , 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14 };
+#endif
 
 int increment(int val, int inc) {
     if (val + inc <= MAX) { return val + inc; }
@@ -35,17 +50,6 @@ int decrement(int val, int dec) {
     if (val - dec >= MIN) { return val - dec; }
     else {return MAX - (val-dec+1)*-1;}
 }
-void pointerincrement(int inc) {
-    int val = (pin - pins);
-    if (val + inc <= MAX) { pin = &pins[val + inc]; }
-    else { pin = &pins[MIN + ((val + inc) % MAX - 1)]; }
-}
-void pointerdecrement(int dec) {
-    int val = (pin - pins);
-    if (val - dec >= MIN) { pin = &pins[val - dec]; }
-    else { pin = &pins[MAX - (val - dec + 1) * -1]; }
-}
-
 
 void setup() {}
 
