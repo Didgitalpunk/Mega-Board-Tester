a44e7137fc784a87cbca6261782622b5efc27217
Didgitalpunk <71613063+Didgitalpunk@users.noreply.github.com>
2023-03-28 23:28:52 +0200

    Update MEGA_board_tester_program.ino
    added leonardo (incomplete) and zero to the list.

--- MEGA_board_tester__program/MEGA_board_tester_program.ino
+++ MEGA_board_tester__program/MEGA_board_tester_program.ino
@@ -4,13 +4,16 @@
 constexpr auto MIN = 0;
 
 #if defined(__AVR_ATmega328P__)
+//arduino uno (R1, R2, R3), arduino nano
 constexpr auto MAX = 19;
 constexpr auto Shift = 4;
 constexpr auto Delay = 100;
 int pins[MAX+1] = {  7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14, 13, 12 , 11, 10, 9, 8 };
 #endif
 
+
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+//arduino Mega
 constexpr auto MAX = 69;
 constexpr auto Shift = 6;
 constexpr auto Delay = 75;
@@ -23,6 +26,20 @@ const int pins[MAX+1] = {
 50, 51, 52, 53,
 69, 68, 67, 66, 65, 64,
 63, 62, 61, 60, 59, 58, 57, 56, 55, 54};
+#endif
+
+
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
+//arduino Leonardo
+#endif
+
+
+#if defined(?)
+//arduino Zero
+constexpr auto MAX = 21;
+constexpr auto Shift = 4;
+constexpr auto Delay = 100;
+int pins[MAX+1] = { 21, 20, 13, 12 , 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 19, 18, 17, 16, 15, 14 };
 #endif
 
 int increment(int val, int inc) {
